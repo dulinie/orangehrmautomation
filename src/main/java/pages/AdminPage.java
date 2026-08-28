469e9778eb5cba29c15e2e00d21f3fe3baa10fcd
dulinie <dulinie@gmail.com>
2026-08-28 13:30:14 -0700

    added loggers

--- src/main/java/pages/AdminPage.java
+++ src/main/java/pages/AdminPage.java
@@ -4,9 +4,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import Utilities.WaitUtils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import base.BaseTest;
 
 public class AdminPage extends BaseTest {
+    private static final Logger logger = LogManager.getLogger(AdminPage.class);
+    
 
     @FindBy(xpath = "//h6[text()='Admin']")
     WebElement adminHeader ;
@@ -17,23 +24,31 @@ public class AdminPage extends BaseTest {
     public AdminPage() {
         // Initialize page elements if needed
         PageFactory.initElements(driver, this);
+        logger.debug("AdminPage object initialized");
     }
 
     public String validateAdminPageTitle() {
-        return driver.getTitle();
+        String title = driver.getTitle();
+        logger.info("Admin page title retrieved: {}", title);
+        return title;
     }
 
     public boolean isAdminHeaderDisplayed() {
-        return adminHeader.isDisplayed();
+        WaitUtils.waitForElementToBeVisible(driver, adminHeader);
+        boolean isDisplayed = adminHeader.isDisplayed();
+        logger.info("Admin header is displayed: {}", isDisplayed);
+        return isDisplayed;
     }
 
     public boolean isAddEmployeeButtonDisplayed() {
-        return addEmployeeButton.isDisplayed();
-
+        boolean isDisplayed = addEmployeeButton.isDisplayed();  
+        logger.info("Add Employee button is displayed: {}", isDisplayed);
+        return isDisplayed;
     }
 
     public AddEmployeePage clickAddEmployeeButton() {
         addEmployeeButton.click();
+        logger.info("Add Employee button clicked, navigating to AddEmployeePage");
         return new AddEmployeePage(); // Return an instance of the AddEmployeePage after clicking the button
     }
 
