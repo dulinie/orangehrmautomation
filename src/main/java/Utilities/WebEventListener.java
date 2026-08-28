5cf2c31ee16eff25418c559f3819f128ed479598
dulinie <dulinie@gmail.com>
2026-08-28 13:28:29 -0700

    Remove the capture screenshot method

--- src/main/java/Utilities/WebEventListener.java
+++ src/main/java/Utilities/WebEventListener.java
@@ -1,6 +1,5 @@
 package Utilities;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -8,42 +7,38 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import base.BaseTest;
 
+public class WebEventListener implements WebDriverListener {
 
-public class WebEventListener extends BaseTest implements WebDriverListener {
+    private static final Logger logger = LogManager.getLogger(WebEventListener.class);
 
     @Override
     public void beforeTo(WebDriver.Navigation navigation, String url) {
-        System.out.println("Navigating to URL: " + url);
+        logger.debug("Navigating to URL: {}", url);
     }
 
     @Override
     public void beforeFindElement(WebDriver driver, By locator) {
-        System.out.println("Searching for element using locator: " + locator);
+        logger.debug("Searching for element using locator: {}", locator);
     }
 
     @Override
     public void beforeClick(WebElement element) {
-        System.out.println("Attempting to click element: " + element.getTagName());
+        logger.debug("Attempting to click element: {}", element.getTagName());
     }
 
     @Override
     public void afterClick(WebElement element) {
-        System.out.println("Successfully clicked the element.");
+        logger.debug("Successfully clicked the element.");
     }
 
     @Override
     public void onError(Object target, Method method, Object[] args, InvocationTargetException error) {
-        System.out.println("Exception occurred: " + error.getCause());
-        try {
-            WaitUtils.takeScreenshotAtEndOfTest(BaseTest.driver);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        logger.error("Exception occurred: {}", error.getCause());
+        ScreenshotUtils.captureScreenshot(BaseTest.driver, "Error_" + System.currentTimeMillis());
     }
-
-
-    
 }
