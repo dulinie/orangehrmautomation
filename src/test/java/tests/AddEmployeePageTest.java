984368e15af591d73ff45f6530894b6b5ae5364f
dulinie <dulinie@gmail.com>
2026-09-03 13:15:29 -0700

    Refactor AdsEmployeePageTest to extend TestBase instead of duplicating setup/teardown

--- src/test/java/tests/AddEmployeePageTest.java
+++ src/test/java/tests/AddEmployeePageTest.java
@@ -1,18 +1,14 @@
 package tests;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import Utilities.DataProviderUtils;
-import Utilities.ScreenshotUtils;
-import base.BaseTest;
 import pages.AddEmployeePage;
 import pages.AdminPage;
 import pages.HomePage;
 import pages.LoginPage;
 
-public class AddEmployeePageTest extends BaseTest {
+public class AddEmployeePageTest extends TestBase {
 
 
     LoginPage loginPage;
@@ -26,13 +22,10 @@ public class AddEmployeePageTest extends BaseTest {
     }
 
     @BeforeMethod   
-    public void setUp() {
-        initialize(); // Initialize WebDriver and open the browser
-               
+    public void addEmployeePageSetUp() {
         loginPage = new LoginPage(); // Create an instance of the LoginPage
         homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password")); // Perform login to navigate to HomePage
         adminPage = new AdminPage(); // Create an instance of the AdminPage
-
         adminPage = homePage.clickAdminMenu(); // Click on the Admin tab to navigate to the AdminPage
         
     }
@@ -49,15 +42,4 @@ public class AddEmployeePageTest extends BaseTest {
         addEmployeePage.addEmployee(urole, ename, status, username, password, confirmPassword); // Fill in the employee details and submit the form
        
     }
-
-    
-    @AfterMethod
-public void tearDown(ITestResult result) {
-    if (result.getStatus() == ITestResult.FAILURE) {
-        ScreenshotUtils.captureScreenshot(driver, result.getName());
-    }
-    if (driver != null) {
-        driver.quit();
-    }
-}
 }
